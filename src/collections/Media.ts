7b44e7238be3b0524e4412c765395e1ecd467487
Jacob Chademwiri <jchademwiri@gmail.com>
2025-02-23 06:36:08 +0200

    feat: add Todos collection and update access controls; refactor styles and imports

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -4,6 +4,10 @@ export const Media: CollectionConfig = {
   slug: 'media',
   access: {
     read: () => true,
+    create: () => true,
+    update: () => true,
+    delete: () => true,
+    
   },
   fields: [
     {
---
+++ src/collections/Todos.ts
@@ -0,0 +1,34 @@
+import { CollectionConfig } from 'payload'
+
+export const Todos: CollectionConfig = {
+  slug: 'todos',
+  access: {
+    read: () => true,
+    create: () => true,
+    update: () => true,
+    delete: () => true,
+  },
+
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'description',
+      type: 'textarea',
+      required: true,
+    },
+    {
+      name: 'media',
+      type: 'upload',
+      relationTo: 'media',
+    },
+    {
+      name: 'completed',
+      type: 'checkbox',
+      defaultValue: false,
+    },
+  ],
+}
