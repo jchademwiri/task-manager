74022085b7b5fae64963cb3402d70c7a72115d38
Jacob Chademwiri <jchademwiri@gmail.com>
2025-02-23 06:53:27 +0200

    feat: implement Todos display on homepage and add Card component for UI

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,5 +1,5 @@
 import { headers as getHeaders } from 'next/headers.js'
-import Image from 'next/image'
+
 import { getPayload } from 'payload'
 import React from 'react'
 import { fileURLToPath } from 'url'
@@ -13,11 +13,29 @@ export default async function HomePage() {
   const payload = await getPayload({ config: payloadConfig })
   const { user } = await payload.auth({ headers })
 
-  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
+  const todos = await payload.find({
+    collection: 'todos',
+    limit: 10,
+  })
 
   return (
-    <main className='' >
-
-    </main>
+    <div className="container mx-auto p-4">
+      <header>
+        <h1 className="text-3xl font-bold mb-4">Hello, {user ? user.email : 'none'}!</h1>
+      </header>
+      <section>
+        <h2 className="text-2xl font-bold mb-4">Todos</h2>
+        <ul>
+          {todos.docs.map((todo) => (
+            <li key={todo.id} className="mb-2">
+              <div>
+                <h3 className='font-bold' >{todo.title}</h3>
+                <p>{todo.description}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
   )
 }
